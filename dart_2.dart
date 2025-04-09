b0284cb7e8d9c59d1492ebd244edc36fcf6f6049
gyeon24 <162951845+gyeon24@users.noreply.github.com>
2025-04-09 13:46:50 +0900

    Update dart_2.dart

--- dart_2.dart
+++ dart_2.dart
@@ -1,53 +1,47 @@
-import 'dart:convert';
 import 'dart:math';
 
-class Student{
+class Student {
   String name = '';
   int kor = 0;
   int eng = 0;
   int math = 0;
   double avg = 0.0;
-  Student(){
+
+  Student(this.name) {
     kor = Random().nextInt(99) + 1;
     eng = Random().nextInt(99) + 1;
     math = Random().nextInt(99) + 1;
   }
-  void setAvg() => avg = (kor + eng + math) / 3.0;
-}
-void main() {
-  var aList = List.empty(growable: true);
-  for(int i=0;i<10;i++){
-    Student a = Student();
-    a.name = AsciiDecoder().convert([65 + i]);
-    aList.add(a);
-  }
-  String strTable = '이름 국어 영어 수학 평균';
-  print(strTable);
-  for(int i=0;i<10;i++){
-    aList[i].setAvg();
-    print(aList[i].name.padLeft(4)+
-        aList[i].kor.toString().padLeft(5)+
-        aList[i].eng.toString().padLeft(5)+
-        aList[i].math.toString().padLeft(5)+
-        aList[i].avg.toStringAsFixed(2).padLeft(8));
-  }
-  print("평균으로 정렬");
-  print(strTable + "  비고");
-  List newList = List.from(aList);
-  newList.sort((a,b) => a.avg.compareTo(b.avg));
 
-  for(var i in newList){
-    String str = i.name.padLeft(4) +
-        i.kor.toString().padLeft(5)+
-        i.eng.toString().padLeft(5)+
-        i.math.toString().padLeft(5)+
-        i.avg.toStringAsFixed(2).padLeft(8);
-    if (i.avg > 50.0){
-      str += "PASS".padLeft(5);
-      print(str);
-    } else{
-      str += "FAIL".padLeft(5);
-      print(str);
-    }
+  void setAvg() {
+    avg = (kor + eng + math) / 3.0;
+  }
+
+  String x() {
+    return avg >= 50.0 ? 'PASS' : 'FAIL';
+  }
+}
+
+void main() {
+  List<Student> students = [];
+  List<String> names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+
+  for (var name in names) {
+    var student = Student(name);
+    student.setAvg();
+    students.add(student);
+  }
+
+  print('이름  국어  영어  수학  평균  비고');
+  for (var student in students) {
+    print('${student.name}   ${student.kor}   ${student.eng}   ${student.math}   ${student.avg.toStringAsFixed(2)}   ${student.x()}');
+  }
+
+  students.sort((a, b) => b.avg.compareTo(a.avg));
+
+  print('\n평균으로 정렬 (내림차순):');
+  print('이름  국어  영어  수학  평균  비고');
+  for (var student in students) {
+    print('${student.name}   ${student.kor}   ${student.eng}   ${student.math}   ${student.avg.toStringAsFixed(2)}   ${student.x()}');
   }
 }
